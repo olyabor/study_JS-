4457ab721a8d9b203e45c38d1fc43392dc481337
Olga Borisenko <olyabor800@gmail.com>
2020-08-12 14:35:55 +0300

    feat: add a string conversion function

--- script/script.js
+++ script/script.js
@@ -4,15 +4,7 @@ let stringFunction = function (argument) {
   if (typeof argument !== 'string') {
     return 'В качестве аргумента передана не строка';
   } else {
-    let start = 0,
-        end = argument.length;
-    while (argument.substr(start, end).startsWith(' ')) {
-      start++;
-    }
-    while (argument.substr(start, end)  .endsWith(' ')) {
-      end--;
-    }
-    argument = argument.substr(start, end);
+    argument = argument.trim();
   return (argument.length > 30) ? argument.substr(0, 30) + '...' : argument;
   }
 };
