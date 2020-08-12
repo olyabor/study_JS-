22dac32ee501da5f3b0d9dca5040f395b479e95d
Olga Borisenko <olyabor800@gmail.com>
2020-08-12 17:02:50 +0300

    feat: add an output of prime numbers

--- script/script.js
+++ script/script.js
@@ -1,16 +1,28 @@
 'use strict';
+//Вывести в консоль только те числа, что начинаются с цифры 2 или 4
+let arr = ['276', '1538', '289', '1111', '476', '358', '67965'];
 
-let stringFunction = function (argument) {
-  if (typeof argument !== 'string') {
-    return 'В качестве аргумента передана не строка';
-  } else {
-    argument = argument.trim();
-  return (argument.length > 30) ? argument.substr(0, 30) + '...' : argument;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i][0] === '2' || arr[i][0] === '4') {
+    console.log(arr[i]);
   }
+}
+//Вывести в столбик все простые числа от 1 до 100
+//Проверить, является ли число простым
+let isPrime = function(n) {
+  let flag = true;
+  for (let i = 2; i <= Math.round(n ** 0.5); i++) {
+    if (i > 10 && (i % 2 === 0 || i % 10 === 5)) {
+      continue;
+    } else if (n % i === 0) {
+      flag = false;
+    }
+  }
+  return flag;
 };
-
-console.log(stringFunction('       Короткая строка   '));
-console.log(stringFunction('                '));
-console.log(stringFunction(23));
-console.log(stringFunction([0, 1]));
-console.log(stringFunction('       Строка, которая содержит более 30 знаков   '));
+//Выыести простые числа в консоль
+for (let i = 2; i < 100; i++) {
+  if (isPrime(i)) {
+    console.log(i + ' делится только на 1 и ' + i);
+  }
+}
