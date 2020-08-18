3d144046a3f6d97e3391dfaa997a43c42c7dfa81
Olga Borisenko <olyabor800@gmail.com>
2020-08-18 19:25:31 +0300

    feat: add a various output of the date and time

--- script/script.js
+++ script/script.js
@@ -1,24 +1,62 @@
 'use strict';
 let main = document.querySelector('.main');
-let date = new Date();
-console.log(date);
-
-let week = [
-  'Понедельник',
-  'Вторник',
-  'Среда',
-  'Четверг',
-  'Пятница',
-  'Суббота',
-  'Воскресенье',
-];
-
-for (let i = 0; i < week.length; i++) {
-  main.innerHTML += `<div class = ${
-      i > 4 ? 'weekend' : 'weekday'
-    }> ${week[i]}</div>`;
-  if (i === date.getDay() - 1) {
-    main.querySelectorAll('div')[i].classList.add('today');
+//Для минут и секунд
+function end1(n) {
+  if (n % 10 === 1 && n !== 11) {
+    return 'a ';
+  } else if ((n % 10 > 4)||(n > 10 && n < 20) || n % 10 === 0) {
+    return ' ';
+  } else {
+    return 'ы ';
   }
 }
+//Для часов
+function end2(n) {
+  if (n % 10 === 1 && n !== 11) {
+    return ' ';
+  } else   if (n % 10 > 4 || (n > 4 && n < 20) || n % 10 === 0) {
+             return 'ов ';
+           } else {
+             return 'а ';
+           }
+}
+
+let writeDataA = () => {
+  let date = new Date();
+  let year = date.getFullYear(),
+  hours = date.getHours(),
+  minutes = date.getMinutes(),
+  seconds = date.getSeconds();
+
+  return 'Сегодня ' +
+    date.toLocaleString('ru', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+    }) + ' ' +
+    year + ' года, ' +
+    hours + ' час' + end2(hours) +
+    minutes + ' минут' + end1(minutes) +
+    seconds + ' секунд' + end1(seconds);
+};
+// let writeDataB = () => {
+//   let date = new Date();
+//   return date.toLocaleDateString() + ' - ' + date.toLocaleTimeString();
+// };
+let addNull = n => n < 10 ? '0' + n : n;
+let writeDataB = () => {
+  let date = new Date();
+  let year = date.getFullYear(),
+    month = date.getMonth(),
+    day = date.getDay(),
+    hours = date.getHours(),
+    minutes = date.getMinutes(),
+    seconds = date.getSeconds();
+  return addNull(day)+'.'+addNull(month)+'.'+year+' - '+addNull(hours)+':'+addNull(minutes)+':'+addNull(seconds);
+};
+
+setInterval(() => {
+  main.innerHTML = `<p>${writeDataA()}</p><p>${writeDataB()}</p>`;
+}, 1000);
+
 
