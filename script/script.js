1f6012d0ba27fbd4acd7f718b84d650a569b8b01
Olga Borisenko <olyabor800@gmail.com>
2020-09-06 15:28:07 +0300

    feat: add animation button

--- script/script.js
+++ script/script.js
@@ -1,16 +1,39 @@
 'use strict';
-let color = document.getElementById('color'),
-  change = document.getElementById('change'),
-  wrapper = document.querySelector('.wrapper');
 
-change.addEventListener('click', function() {
-  let randomColor = '#' +
-    Math.floor(Math.random() * 256).toString(16) +
-    Math.floor(Math.random() * 256).toString(16) +
-    Math.floor(Math.random() * 256).toString(16);
-  wrapper.style.backgroundColor = randomColor;
-  change.style.color = randomColor;
-  color.textContent = randomColor;
+const img = document.querySelector('.img'),
+  start = document.querySelector('.start'),
+  reset = document.querySelector('.reset');
+
+let count = 0,
+  flyInterval;
+
+let flyAnimate = function() {
+  flyInterval = requestAnimationFrame(flyAnimate);
+  count++;
+  if (count < 18000) {
+    img.style.cssText = `top: ${200 * Math.cos(count / 100) + 200}px;
+    left: ${200 * Math.sin(count / 100) + 600}px`;
+  } else {
+    count = 0;
+  }
+};
+
+let animate = true;
+
+start.addEventListener('click', () => {
+  if (animate) {
+    flyInterval = requestAnimationFrame(flyAnimate);
+    animate = false;
+  } else {
+    cancelAnimationFrame(flyInterval);
+    animate = true;
+  }
 });
 
+reset.addEventListener('click', () => {
+  animate = true;
+  cancelAnimationFrame(flyInterval);
+  count = 0;
+  img.style.cssText = `left: 600px; top: 400px`;
+});
 
